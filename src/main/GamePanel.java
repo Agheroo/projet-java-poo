2f0bb80c06a04cab2e7dbd08d210105acd3ff8e5
Zouaouak <super.youness83@gmail.com>
2023-10-16 14:23:10 +0200

    Test

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -45,6 +45,7 @@ public class GamePanel extends JPanel implements  Runnable{
         while (gameThread!= null){
 
             long currentTime=System.nanoTime();
+            sys.
 
             update();
 
