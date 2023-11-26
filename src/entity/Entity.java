125867a8de014f2f1f723f7fbfff1e0960c8bd4a
Renan <reauxrenan@outlook.fr>
2023-11-26 21:21:04 +0100

    Deleted useless files and commented debug lines

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -80,7 +80,6 @@ public class Entity {
 
     public void draw(Graphics2D g2, int screenX, int screenY){
         BufferedImage image=null;
-        System.out.println(worldX + " " + worldY);
         if(speed == 0){ //IDLE ANIMATIONS
             for(int i=0;i<_spriteCntMax;i++){
                 switch (facing) {
