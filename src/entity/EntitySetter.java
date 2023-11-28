e8f76d5f21b5388074938da9d0131c1ea0bf9d11
Romuald <moisan.romuald@gmail.com>
2023-11-28 11:07:17 +0100

    Add Chest, Key and Door objects

--- src/entity/EntitySetter.java
+++ src/entity/EntitySetter.java
@@ -11,16 +11,17 @@ public class EntitySetter {
     public void setObject() {
         gp.obj[0] = new OBJ_Key();
         int tileSize = 16;
-        gp.obj[0].worldX = 32* tileSize;
-        gp.obj[0].worldY = 30* tileSize;
+        int scale = 3;
+        gp.obj[0].worldX = 13* tileSize*scale;
+        gp.obj[0].worldY = 13* tileSize*scale;
 
         gp.obj[1] = new OBJ_Door();
-        gp.obj[1].worldX = 36* tileSize;
-        gp.obj[1].worldY = 30* tileSize;
+        gp.obj[1].worldX = 14* tileSize*scale;
+        gp.obj[1].worldY = 13* tileSize*scale;
 
         gp.obj[2] = new OBJ_Chest();
-        gp.obj[2].worldX = 40* tileSize;
-        gp.obj[2].worldY = 30* tileSize;
+        gp.obj[2].worldX = 15* tileSize*scale;
+        gp.obj[2].worldY = 13* tileSize*scale;
     }
 }
 
