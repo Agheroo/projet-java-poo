ec3ae418e7f405df45adda4b8cc3fc1b04d1ae99
Renan <reauxrenan@outlook.fr>
2023-12-05 23:49:14 +0100

    Added UI package with HUD class

---
+++ src/UI/ChoiceButton.java
@@ -0,0 +1,39 @@
+package UI;
+
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class ChoiceButton {
+    public int width, height;
+    private Textbox _textBox;
+    private BufferedImage _bgTexture;
+    
+    public ChoiceButton(int w,int h, String title, String fontName, Color fontColor){
+        this.width = w; this.height = h;
+
+        _textBox = new Textbox(title,fontName,w,h,fontColor);
+
+        loadTexture();
+    }
+
+    private void loadTexture(){
+        try{
+            _bgTexture = ImageIO.read(new FileInputStream("res/hud/bg.png"));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2, int x, int y){
+        g2.drawImage(_bgTexture,x,y,80*4,40*4,null);
+        _textBox.draw(g2,x,y);
+    }
+}
---
+++ src/UI/HUD.java
@@ -0,0 +1,55 @@
+package UI;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class HUD {
+    public enum MenuType {WELCOME, PAUSE, FIGHT, SHOP}
+
+    private int _nbButtons;
+    private ChoiceButton[] _buttons;
+    private MenuType _type;
+    //private KeyHandler keyH = KeyHandler.getInstance();
+    //private Cursor _cursor;
+
+
+    public final int screenSizeWidth = 125*3;
+    public final int screenSizeHeight = 65*3;
+
+    public HUD(MenuType type){
+        _type = type;
+        switch(_type){
+            case WELCOME:
+                _nbButtons = 2;
+                break;
+            case PAUSE:
+                _nbButtons = 3;
+                break;
+            case FIGHT:
+                _nbButtons = 6;
+                break;
+            case SHOP:
+                _nbButtons = 4;
+                break;
+        }
+
+        _buttons = new ChoiceButton[_nbButtons];
+        for(int i =0; i<_nbButtons ; i++){
+            _buttons[i] = new ChoiceButton(80*4,40*4, "Titre "+i, "Arial",Color.black);
+            
+        }
+    }
+
+
+    public void draw(Graphics2D g2, int screenWidth, int screenHeight){
+        //g2.drawImage(_image, (800 - screenSizeWidth)/2,(600 - screenSizeHeight)/2,screenSizeWidth, screenSizeHeight, null);
+
+        for(int i=0;i < _nbButtons; i++){
+            _buttons[i].draw(g2,(screenWidth - _buttons[i].width)/2, (screenHeight - _buttons[i].height)/2 );
+        }
+    }
+
+    public void update(double dt){
+        
+    }
+}
