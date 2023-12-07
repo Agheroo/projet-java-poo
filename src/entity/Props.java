dc6090c9b4dae9f715c6045cfd0db6cb7a684799
Romuald <moisan.romuald@gmail.com>
2023-12-07 22:08:56 +0100

    Add objects (Door, Key and Chest) on the map

--- src/entity/Props.java
+++ src/entity/Props.java
@@ -8,11 +8,11 @@ import java.awt.image.BufferedImage;
 /**
  * The Props class represents in-game props with properties such as image, name, and position.
  */
-public class Props {
+public class Props extends Entity{
     public BufferedImage image; // Image representing the prop
     public String name; // Name of the prop
 
-    public int worldX, worldY; // Position of the prop in the game world
+
     public final int screenWidth = 800; // Screen width of the game window
     public final int screenHeight = 600; // Screen height of the game window
 
@@ -31,14 +31,12 @@ public class Props {
         int screenX = worldX - world.player.worldX + playerScreenX;
         int screenY = worldY - world.player.worldY + playerScreenY;
 
-        int tileSize = 16; // Size of a tile in the game world
-        int scale = 6; // Scaling factor
 
         // Check if the prop is within the visible screen region around the player
-        if (worldX + tileSize * scale > world.player.worldX - playerScreenX
-                && worldX - tileSize * scale < world.player.worldX + playerScreenX
-                && worldY + tileSize * scale > world.player.worldY - playerScreenY
-                && worldY - tileSize * scale < world.player.worldY + playerScreenY) {
+        if (worldX + _tileSize * _scale > world.player.worldX - playerScreenX
+                && worldX - _tileSize * _scale < world.player.worldX + playerScreenX
+                && worldY + _tileSize * _scale > world.player.worldY - playerScreenY
+                && worldY - _tileSize * _scale < world.player.worldY + playerScreenY) {
             // Draw the prop on the screen
             g2.drawImage(image, screenX, screenY, world.player.screenSize, world.player.screenSize, null);
         }
